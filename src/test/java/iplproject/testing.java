be15c6d3b0e7ef5bb2062f47c41816c6d0e32d55
User <User@Sweety>
2022-07-21 22:39:22 +0530

    My first Repo

---
+++ src/test/java/iplproject/testing.java
@@ -0,0 +1,24 @@
+package iplproject;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class testing {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testLogin() {
+	
+	}
+
+}
